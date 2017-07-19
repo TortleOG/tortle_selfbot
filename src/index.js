8e50355b5592e845ea0ae2950d6cce3259c19fd3
Adam Mahood <mahood.adam@gmail.com>
2017-07-19 12:27:21 -0400

    [PATCH] Basic Functionality
    Adds basic functionality for the bot, with a ping command and ready event. Also changes ESLint rules and adds to config

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+client.config = require("../settings.json");
+
+client.on("ready", () => {
+  console.log("Selfbot initalized. Ready to serve.");
+});
+
+client.on("message", (msg) => {
+  if (msg.author.id !== client.user.id) return;
+  else if (!msg.content.startsWith(client.config.prefix)) return;
+
+  if (msg.content.startsWith(`${client.config.prefix}ping`)) {
+    msg.delete();
+    return msg.channel.send("Ping!").then(m => m.edit(`Pong! Client Latency: **${m.createdTimestamp - msg.createdTimestamp}ms**. API Latency: **${Math.round(client.ping)}ms**`));
+  }
+});
+
+client.login(client.config.token);
+
+process.on("uncaughtException", (err) => {
+  const errorMsg = err.stack.replace(new RegExp(`${__dirname}\/`, "g"), "./");
+  console.error("Uncaught Exception: ", errorMsg);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.error("Uncaught Promise Error: ", err);
+});
