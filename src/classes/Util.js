40d386966fc43c50186382251f9f42792a453103
Adam Mahood <mahood.adam@gmail.com>
2017-08-04 17:02:40 -0400

    [MINOR] Another Args Rewrite
    - Removes useless return of object with only 1 property
    - Rewrites all other args to use returned array instead of obj.array

--- src/classes/Util.js
+++ src/classes/Util.js
@@ -1,57 +1,55 @@
 module.exports = class Util {
   static async validateUsage(cmd, usage) {
-    if (usage === "" || !usage) return true;
-    usage = usage.split(" ");
-
-    return usage.forEach((arg) => {
-      if (arg[0] === "<") {
-        if (arg[arg.length - 1] === ">") return true;
+    return usage !== "" && usage ? (usage = usage.split(" "), usage.forEach((c) => { // eslint-disable-line
+      if (c[0] === "<") {
+        if (c[c.length - 1] === ">") return !0;
         throw `Invalid usage statment at cmd: ${cmd.help.name}`;
-      } else if (arg[0] === "[") {
-        if (arg[arg.length - 1] === "]") return true;
+      } else if (c[0] === "[") {
+        if (c[c.length - 1] === "]") return !0;
         throw `Invalid usage statement at cmd: ${cmd.help.name}`;
       }
       throw `Invalid usage statement at cmd: ${cmd.help.name}`;
-    });
+    })) : !0;
   }
 
   static parseUsage(usage) {
     if (usage === "" || !usage) return null;
-    const obj = { args: [] };
-
+    const args = [];
     usage = usage.split(" ");
-
-    for (let i = 0; i < usage.length; i++) {
-      let required = false;
+    for (let i = 0, req = !1; i < usage.length; i++) {
       usage[i] = usage[i].split(":");
-      if (usage[i][0][0] === "<") required = true;
-      obj.args.push({ name: usage[i][0].slice(1), type: usage[i][1].slice(0, -1), required });
+      if (usage[i][0][0] === "<") req = !0;
+      args.push({
+        name: usage[i][0].slice(1),
+        type: usage[i][1].slice(0, -1),
+        required: req,
+      });
+      req = !1;
     }
-
-    return obj;
+    return args;
   }
 
   static async validateArgs(parsed, args) {
-    for (let i = 0; i < parsed.args.length; i++) {
-      if (parsed.args[i].required && (args[i] === "" || args[i] === undefined)) throw `Missing required argument ${parsed.args[i].name}`;
+    for (let i = 0; i < parsed.length; i++) {
+      if (parsed[i].required && (args[i] === "" || args[i] === undefined)) throw `Missing required argument ${parsed.args[i].name}`;
       try {
-        if (parsed.args[i].type === "str") Util.validateStr(args[i]);
-        else if (parsed.args[i].type === "int") Util.validateInt(args[i]);
-        else if (parsed.args[i].type === "float") Util.validateFloat(args[i]);
-        else if (parsed.args[i].type === "bool") Util.validateBool(args[i]);
+        if (parsed[i].type === "str") Util.validateStr(args[i]);
+        else if (parsed[i].type === "int") Util.validateInt(args[i]);
+        else if (parsed[i].type === "float") Util.validateFloat(args[i]);
+        else if (parsed[i].type === "bool") Util.validateBool(args[i]);
       } catch (err) {
-        throw `${parsed.args[i].name} ${err}`;
+        throw `${parsed[i].name} ${err}`;
       }
     }
   }
 
   static parseArgs(parsed, args) {
     const newArgs = [];
-    for (let i = 0; i < parsed.args.length; i++) {
-      if (parsed.args[i].type === "str") newArgs.push(args[i]);
-      else if (parsed.args[i].type === "int") newArgs.push(parseInt(args[i]));
-      else if (parsed.args[i].type === "float") newArgs.push(parseFloat(args[i]));
-      else if (parsed.args[i].type === "bool") newArgs.push(args[i]);
+    for (let i = 0; i < parsed.length; i++) {
+      if (parsed[i].type === "str") newArgs.push(args[i]);
+      else if (parsed[i].type === "int") newArgs.push(parseInt(args[i]));
+      else if (parsed[i].type === "float") newArgs.push(parseFloat(args[i]));
+      else if (parsed[i].type === "bool") newArgs.push(args[i]);
     }
     return newArgs;
   }
@@ -63,16 +61,16 @@ module.exports = class Util {
 
   static validateInt(input) {
     if (isNaN(input)) throw "argument must be an integer.";
-    return true;
+    return !0;
   }
 
   static validateFloat(input) {
     if (isNaN(input)) throw "argument must be a floating point value.";
-    return true;
+    return !0;
   }
 
   static validateBool(input) {
     if (input !== "true" && input !== "false") throw "argument must be 'true' or 'false'.";
-    return true;
+    return !0;
   }
 };
--- src/events/message.js
+++ src/events/message.js
@@ -7,27 +7,27 @@ module.exports = async (client, msg) => {
   const command = msg.content.split(/\s+/g)[0].slice(client.config.prefix.length).toLowerCase();
   const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command));
 
-  let args = !cmd.help.usageDelim || cmd.help.usageDelim === "" ? msg.content.split(/\s+/g).slice(1) : msg.content.split(/\s+/g).slice(1).join(" ").split(cmd.help.usageDelim);
+  if (cmd && cmd.conf.enabled) {
+    let args = !cmd.help.usageDelim || cmd.help.usageDelim === "" ? msg.content.split(/\s+/g).slice(1) : msg.content.split(/\s+/g).slice(1).join(" ").split(cmd.help.usageDelim);
+    try {
+      // No usage, run command
+      if (cmd.help.usage === "") return cmd.run(client, msg);
 
-  try {
-    // CMD Usage parsing and validation
-    await validateUsage(cmd, cmd.help.usage);
-    const usage = parseUsage(cmd.help.usage);
-    if (usage === null && cmd.conf.enabled) return cmd.run(client, msg);
+      // CMD Usage parsing and validation
+      await validateUsage(cmd, cmd.help.usage);
+      const usage = parseUsage(cmd.help.usage);
 
-    // If only one argument, pass all args as one array
-    args = usage.args.length === 1 ? [args.join(" ")] : args;
+      // If only one argument, pass all args as one array
+      args = usage.length === 1 ? [args.join(" ")] : args;
 
-    // Argument validation and parsing
-    await validateArgs(usage, args);
-    const params = parseArgs(usage, args);
+      // Argument validation and parsing
+      await validateArgs(usage, args);
+      const params = parseArgs(usage, args);
 
-    // If cmd exists and is enabled run the command
-    if (cmd && cmd.conf.enabled) return cmd.run(client, msg, params);
-  } catch (err) {
-    if (err === `Invalid usage statement at cmd: ${cmd.help.name}`) return msg.channel.send(`\`\`\`js\n${err}\`\`\``);
-    else if (err.startsWith("Missing required argument")) return msg.channel.send(`\`\`\`js\n${err}\`\`\``);
-    else if (err.search("argument") !== -1) return msg.channel.send(`\`\`\`js\n${err}\`\`\``);
-    return msg.channel.send(`\`\`\`js\n${err.stack}\`\`\``);
+      // If cmd exists and is enabled run the command
+      return cmd.run(client, msg, params);
+    } catch (err) {
+      return msg.channel.send(err.stack ? `\`\`\`js\n${err.stack}\`\`\`` : `\`\`\`js\n${err}\`\`\``);
+    }
   }
 };
